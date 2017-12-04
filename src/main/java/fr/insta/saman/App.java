77adfbb37c6c3cdd6f76c0f03e413e4d4818c9bd
saman1122 <saman11@hotmail.fr>
2017-12-04 11:04:05 +0100

    first commit

---
+++ src/main/java/fr/insta/saman/App.java
@@ -0,0 +1,13 @@
+package fr.insta.saman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
